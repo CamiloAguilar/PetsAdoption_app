fe690f7467e73ff1500c0e04b3f205a2752df35f
Camilo Aguilar <aguilarcamiloa@gmail.com>
2019-06-29 09:07:21 -0500

    add icons and pet review

---
+++ lib/pet_review.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PetReview extends StatelessWidget{
+
+  String path_image = "assets/img/Ozzy.jpeg";
+  String pet_name = "Ozzy";
+  String pet_breed = "Chader Retriever";
+  String pet_age = "1 year";
+  String pet_distance = "Distance: 1.2 Km";
+
+  //Generamos el constructor
+  PetReview(this.path_image, this.pet_name, this.pet_breed, this.pet_age, this.pet_distance);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+
+    return null;
+  }
+
+}
